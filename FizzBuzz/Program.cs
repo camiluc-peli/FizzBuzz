b4486e27b923c8aa8b004a542aaa59ffdbb47575
Camila <camiluc.peli@gmail.com>
2019-08-14 16:27:54 +0200

    Some refactoring

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,21 +10,19 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            var results = FizzBuzzIsFizzBuzzWithLinq(1, 100);
+            var results = FizzBuzzReadable(1, 100);
 
             Console.WriteLine(
                     string.Join(Environment.NewLine, results).ToArray()
                 );
 
-
-
             Console.Read();
         }
 
         /// <summary>
-        /// Because the FizzBuzz problem is what it is, 
-        /// and any other added specifications or extensions will make FizzBuzz stop being FizzBuzz and becoming another problem, 
-        /// this is the KISS implementation.
+        /// FizzBuzz implementation 
+        /// Here, I understand that if both conditions are true we print FizzBuzz, because conditions are true independently. 
+        /// FizzBuzz is not a new value, it's the result of the two values of both conditions being printed.
         /// </summary>
         /// <param name="minimumValue">First number in the sequence.</param>
         /// <param name="maximumValue">Last number in the sequence.</param>
@@ -59,14 +57,12 @@ namespace FizzBuzz
         }
 
         /// <summary>
-        /// Because the FizzBuzz problem is what it is, 
-        /// and any other added specifications or extensions will make FizzBuzz stop being FizzBuzz and becoming another problem, 
-        /// this is the KISS implementation with Linq.
+        /// FizzBuzz implementation with Linq where every condition is explicit.
         /// </summary>
         /// <param name="minimumValue">First number in the sequence.</param>
         /// <param name="maximumValue">Last number in the sequence.</param>
         /// <returns></returns>
-        public static List<string> FizzBuzzIsFizzBuzzWithLinq(int minimumValue, int maximumValue)
+        public static List<string> FizzBuzzRedeableWithLinq(int minimumValue, int maximumValue)
         {
             return Enumerable.Range(minimumValue, maximumValue)
                 .Select(
@@ -76,6 +72,39 @@ namespace FizzBuzz
                                 value % 5 == 0 ? "Buzz" :
                                     value.ToString(CultureInfo.InvariantCulture)))
                 .ToList();
+        }
+
+        /// <summary>
+        /// FizzBuzz solutioon where every condition is explicit.
+        /// </summary>
+        /// <param name="minimumValue"></param>
+        /// <param name="maximumValue"></param>
+        /// <returns>List of FizzBuzz elements.</returns>
+        public static List<string> FizzBuzzReadable(int minimumValue, int maximumValue)
+        {
+            List<string> resultValues = new List<string>();
+
+            foreach (var value in Enumerable.Range(minimumValue, maximumValue))
+            {
+                if(value % 3 == 0 && value % 5 == 0)
+                {
+                    resultValues.Add("FizzBuzz");
+                }
+                else if(value % 3 == 0)
+                {
+                    resultValues.Add("Fizz");
+                }
+                else if(value % 5 == 0)
+                {
+                    resultValues.Add("Buzz");
+                }
+                else
+                {
+                    resultValues.Add(value.ToString(CultureInfo.InvariantCulture));
+                }
+            }
+
+            return resultValues;
         }
     }
 }
