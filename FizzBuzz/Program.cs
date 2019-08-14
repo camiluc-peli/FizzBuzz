d6f1caf994321bb2d7a9613926ffde1bf8af67ed
Camila <camiluc.peli@gmail.com>
2019-08-14 15:29:08 +0200

    Quick fix

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,7 +10,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            var results = FizzBuzzIsFizzBuzz(1, 100);
+            var results = FizzBuzzIsFizzBuzzWithLinq(1, 100);
 
             Console.WriteLine(
                     string.Join(Environment.NewLine, results).ToArray()
@@ -73,7 +73,7 @@ namespace FizzBuzz
                     value => (
                         value % 3 == 0 && value % 5 == 0 ? "FizzBuzz" :
                             value % 3 == 0 ? "Fizz" :
-                                value % 5 == 0 ? "Buzz : " :
+                                value % 5 == 0 ? "Buzz" :
                                     value.ToString(CultureInfo.InvariantCulture)))
                 .ToList();
         }
